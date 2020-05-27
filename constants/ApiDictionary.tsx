9c59c115ba8c722729efcb98774faec52186a7a2
Fillette97 <48556597+Fillette97@users.noreply.github.com>
2020-05-27 20:02:02 +0200

    Api Dictionary constant geadd

---
+++ constants/ApiDictionary.tsx
@@ -0,0 +1,8 @@
+export default {
+    //hier een lijst van alle apidestinations, gebruikt dit incombinatie met de httpclient :)
+    login: {destination: '/api/users/login', type: 'POST'},
+    addPost: {destination: '/api/users/add/post', type: 'POST'},
+
+    //dit is de default ip van de api server
+    apiServer: 'http://localhost:3000',
+};
