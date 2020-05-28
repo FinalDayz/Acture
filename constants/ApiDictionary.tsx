c3b016f8f8cf05999eb20cc634c9cfd19d87675e
FinalDayz <steef2000d@gmail.com>
2020-05-28 14:17:23 +0200

    standard enviromentVars

--- constants/ApiDictionary.tsx
+++ constants/ApiDictionary.tsx
@@ -1,8 +1,12 @@
+import enviromentsVars from "./environmentVars";
+
 export default {
     //hier een lijst van alle apidestinations, gebruikt dit incombinatie met de httpclient :)
-    login: {destination: '/api/users/login', type: 'POST'}, register: {destination: '/api/users/register', type: 'POST'},
+    login: {destination: '/api/users/login', type: 'POST'},
+    register: {destination: '/api/users/register', type: 'POST'},
+    inactive: {},
 
     //dit is de defaults van de api server (verander dit naar je home pc)
-    apiServer: 'http://192.168.178.248:3000',
+    apiServer: enviromentsVars.address + ":" + enviromentsVars.port,
     timeoutTimings: 7000, //in miliseconds
 };
---
+++ constants/environmentVars.tsx
@@ -0,0 +1,4 @@
+export default class enviromentsVars {
+    static address: string = 'http://192.168.137.1';
+    static port: number = 3000;
+}
