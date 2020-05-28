3ed61bee8e375b3254c8219fdf8538ccf7a9f722
AnthonySchuijlenburg <a.schuijlenburg@gmail.com>
2020-05-28 14:56:20 +0200

    Allert message and returning to loginscreen

--- constants/ApiDictionary.tsx
+++ constants/ApiDictionary.tsx
@@ -3,6 +3,6 @@ export default {
     login: {destination: '/api/users/login', type: 'POST'}, register: {destination: '/api/users/register', type: 'POST'},
 
     //dit is de defaults van de api server (verander dit naar je home pc)
-    apiServer: 'http://192.168.178.248:3000',
+    apiServer: 'http://192.168.178.55:3000',
     timeoutTimings: 7000, //in miliseconds
 };
