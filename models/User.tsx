346948e9f6ec77066440400dd55f9c1a99de4f60
Stefan <steef2000d@gmail.com>
2020-06-09 17:19:28 +0200

    Singleton for User

--- models/User.tsx
+++ models/User.tsx
@@ -1,20 +1,38 @@
 import { UserRole } from "./UserRole";
 
 export class User {
+    private static loggedInUser: User;
+
     firstname: string = '';
     lastname: string = '';
-    private static userId: number = 1;
+    userId: number = 1;
     address: string = '';
     tussenvoegsel: string = '';
     register: Date = new Date();
     unregister: Date = new Date();
     password: string = '';
-    private static role: UserRole = UserRole.user;
+    role: UserRole = UserRole.user;
     email: string = '';
     image: string = '';
     telephone: number = 0;
     description: string = '';
     activivated: boolean = false;
+
+    static getLoggedInUser(): User
+    {
+        if(User.loggedInUser === undefined) {
+            User.loggedInUser = new User();
+        }
+        return User.loggedInUser;
+    }
+
+    public static getRole() {
+        return User.getLoggedInUser().role;
+    }
+
+    public static getUserId() {
+        return User.getLoggedInUser().userId;
+    }
 
     public getFullName() {
         return this.firstname +
@@ -22,15 +40,7 @@ export class User {
             + " " + this.lastname;
     }
 
-    public static getRole() {
-        return User.role;
-    }
-
-    public static getUserId() {
-        return User.userId;
-    }
-
-    constructor(firstname: string, insertion: string, lastname: string, email: string, password: string){
+    constructor(firstname: string = '', insertion: string = '', lastname: string = '', email: string = '', password: string = ''){
         this.firstname = firstname;
         this.tussenvoegsel = insertion;
         this.lastname = lastname;
