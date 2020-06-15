6532c0bf45938e2795c65d371fe3a42d24fc1e0a
Stan <hogenboomstan@gmail.com>
2020-06-15 13:20:51 +0200

    Update HelpScreen.tsx

--- screens/HelpScreen.tsx
+++ screens/HelpScreen.tsx
@@ -2,55 +2,90 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+import {bodyfull} from '../components/HttpClient';
+import ApiDictionary from '../constants/ApiDictionary';
 import colors from '../constants/colors';
 import HeaderButton from '../components/HeaderButton';
+import {PostModel} from '../models/PostModel';
 
-export interface Props {}
+export default class HelpScreen extends React.Component<any, any> {
 
-const HelpScreen = (props: Props) => {
-    return(
-        <View style={styles.screen}>
-            <Text>Help placeholder</Text>
-        </View>
-    );
-};
+    state = {
+        isLoading: false,
+        data: [],
+        offset: 0
+    };
 
-//options for header bar. Default options are in the navigator.
-HelpScreen.navigationOptions = (navData:any) => {
-    return {
-        headerTitle: 'Help',
-        headerRight: () => (
-            <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                <Item 
-                title='profile'
-                iconName='md-person' //TODO: change to profile picture
-                onPress={() => {
-                    navData.navigation.navigate('Profile');
-                }}/>
-            </HeaderButtons>
-        ),
-        headerLeft: () => (
-            <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                <Item 
-                    title='menu'
-                    iconName='md-menu'
+    constructor(props: any) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.getGuides()
+    }
+
+    getGuides() {
+        if(!this.state.isLoading) {
+            
+            this.state.isLoading = true;
+            bodyfull(ApiDictionary.getFeed, {
+                offs: this.state.offset //offset for loading more posts
+            })
+            .then(
+                (result: {data:Array<PostModel>}) => {
+                    this.setState({data: result.data})
+                })
+            .catch ((error) => {
+                console.log(error);
+            })
+            this.setState({isLoading : false});
+        }
+    }
+
+    render() {
+        return(
+            <View style={this.styles.screen}>
+                <Text>Help placeholder</Text>
+            </View>
+        );
+    }
+
+
+    //options for header bar. Default options are in the navigator.
+    static navigationOptions = (navData:any) => {
+        return {
+            headerTitle: 'Guides',
+            headerRight: () => (
+                <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                    <Item 
+                    title='profile'
+                    iconName='md-person' //TODO: change to profile picture
                     onPress={() => {
-                        navData.navigation.toggleDrawer();
-                    }} 
-                />
-            </HeaderButtons>
-        )
-    }
-};
+                        navData.navigation.navigate('Profile');
+                    }}/>
+                </HeaderButtons>
+            ),
+            headerLeft: () => (
+                <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                    <Item 
+                        title='menu'
+                        iconName='md-menu'
+                        onPress={() => {
+                            navData.navigation.toggleDrawer();
+                        }} 
+                    />
+                </HeaderButtons>
+            )
+        }
+    };
 
-const styles = StyleSheet.create ({
-    screen: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize: 30,
-        backgroundColor: colors.backgroundPrimary
-    }
-});
-
-export default HelpScreen;
+    styles = StyleSheet.create ({
+        screen: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: 30,
+            backgroundColor: colors.backgroundPrimary
+        }
+    });
+}    
