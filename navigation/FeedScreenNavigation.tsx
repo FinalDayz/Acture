fa3b47c5619411c5e9f723cd18350716427735e1
Fillette97 <48556597+Fillette97@users.noreply.github.com>
2020-06-22 11:30:28 +0200

    laatste merge gefixt

--- navigation/FeedScreenNavigation.tsx
+++ navigation/FeedScreenNavigation.tsx
@@ -24,7 +24,7 @@ const FeedScreenNavigation = createStackNavigator(
         },
         userPrivacyScreen: {
             screen: userPrivacyScreen
-        }
+        },
         Attendance: {
             screen: AttendanceScreen
         }
